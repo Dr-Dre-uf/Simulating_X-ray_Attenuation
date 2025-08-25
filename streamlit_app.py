3602f141bfcff03d1ba8e904e4b576092a137c5a
Dr-Dre-uf <deandrecoleman@ufl.edu>
2025-08-25 12:35:27 +0000

    update circles

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,20 +5,28 @@ def generate_and_display_image():
     """Generates and displays the simulated X-ray attenuation image."""
     # Create a grayscale image with a gray background
     image = np.ones((100, 300), dtype=np.uint8) * 100
+
     # Two circular, dark spots
     center1 = (75, 50)
+    radius1 = 30
     center2 = (225, 50)
-    radius = 30
+    radius2 = 35  # Increased radius for the second circle
+    inner_radius = 15 #Radius of inner light circle
 
-    for y in range(100):
-        for x in range(300):
-            dist1 = np.sqrt((y - center1[1])**2 + (x - center1[0])**2)
-            dist2 = np.sqrt((y - center2[1])**2 + (x - center2[0])**2)
+    # Create a mask for the first circle
+    y, x = np.ogrid[-center1[1]:100 - center1[1], -center1[0]:300 - center1[0]]
+    mask1 = x*x + y*y <= radius1*radius1
+    image[mask1] = 30
 
-            if dist1 <= radius:
-                image[y, x] = 30
-            if dist2 <= radius:
-                image[y, x] = 30
+    # Create a mask for the second circle (outer dark circle)
+    y, x = np.ogrid[-center2[1]:100 - center2[1], -center2[0]:300 - center2[0]]
+    mask2 = x*x + y*y <= radius2*radius2
+    image[mask2] = 30
+
+    #Create a mask for the inner light circle
+    y, x = np.ogrid[-center2[1]:100 - center2[1], -center2[0]:300 - center2[0]]
+    mask_inner = x*x + y*y <= inner_radius*inner_radius
+    image[mask_inner] = 100 #reset to background color
 
     # White rectangle in center of image
     rect_top_left = (140, 20)
